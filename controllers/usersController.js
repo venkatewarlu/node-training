f6710d482a906336a8c3c8a9a62648157c2d7279
venkateswarlu <nallapadu.venkateswarlu@gmail.com>
2019-04-03 10:07:37 +0530

    users management

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,22 +2,45 @@ const router = require('express').Router();
 
 const userService = require('../services/usersService');
 
-router.get('/', (req, res) => {
-    const users = userService.getUsers()
+router.get('/', async (req, res) => {
+    const users = await userService.getUsers()
 
 
     res.json({ users: users })
 })
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    console.log(`User id is ${id}`);
-    res.json({ message: 'User Id is denied' })
+
+
+// router.get('/:id', (req, res) => {
+//     const id = req.params.id
+//     console.log(`User id is ${id}`);
+//     res.json({ message: 'User Id is denied' })
+// })
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const users = await userService.getUsersByID(parseInt(id))
+    res.json({ users: users });
+
 })
 
-router.post('/', (req, res) => {
-    console.log(JSON.stringify(req.body, undefined, 4));
-    res.json({ message: "Users post method successful" })
-})
+// router.post('/', (req, res) => {
+//     console.log(JSON.stringify(req.body, undefined, 4));
+//     res.json({ message: "Users post method successful" })
+// })
 
+
+
+router.post('/', async (req, res) => {
+    try {
+        // const { userName, manufacturerID } = req.params;
+        await userService.insert(req.body);
+        res.json({ message: "User Successfully added" })
+
+    }
+    catch (err) {
+        res.status(422).json({ message: err });
+    }
+
+})
 module.exports = router;
--- services/usersService.js
+++ services/usersService.js
@@ -1,102 +1,128 @@
-exports.getUsers = () => {
-    return [
-        {
-            firstName: 'Venkat',
-            lastName: 'Puttamsetti',
-            email: 'venkat.p@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'Inactive'
-        },
+// exports.getUsers = () => {
+//     return [
+//         {
+//             firstName: 'Venkat',
+//             lastName: 'Puttamsetti',
+//             email: 'venkat.p@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'Inactive'
+//         },
 
-        {
-            firstName: 'Sukumar',
-            lastName: 'kuchana',
-            email: 'sukumar@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'active'
-        },
-        {
-            firstName: 'Jyothi',
-            lastName: 'Saikam',
-            email: 'jyothi@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'Inactive'
-        },
-        {
-            firstName: 'Cinni',
-            lastName: 'Thomas',
-            email: 'cinni@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'Inactive'
-        },
-        {
-            firstName: 'Pradeep',
-            lastName: 'Kumar',
-            email: 'Pradeep@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'active'
+//         {
+//             firstName: 'Sukumar',
+//             lastName: 'kuchana',
+//             email: 'sukumar@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'active'
+//         },
+//         {
+//             firstName: 'Jyothi',
+//             lastName: 'Saikam',
+//             email: 'jyothi@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'Inactive'
+//         },
+//         {
+//             firstName: 'Cinni',
+//             lastName: 'Thomas',
+//             email: 'cinni@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'Inactive'
+//         },
+//         {
+//             firstName: 'Pradeep',
+//             lastName: 'Kumar',
+//             email: 'Pradeep@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'active'
 
-        },
-        {
-            firstName: 'Subha',
-            lastName: 'Vellapandi',
-            email: 'subha@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'active'
-        },
-        {
-            firstName: 'Shruthi',
-            lastName: 'Muthu',
-            email: 'Shruthi@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'Inactive'
-        },
-        {
-            firstName: 'Manisha',
-            lastName: 'Akula',
-            email: 'Manisha@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'Inactive'
-        },
-        {
-            firstName: 'Priyanka',
-            lastName: 'Chittibabu',
-            email: 'priyanka@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'Inactive'
-        },
-        {
-            firstName: 'Mohana',
-            lastName: 'Manohar',
-            email: 'mohana@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'active'
-        },
-        {
-            firstName: 'Rama',
-            lastName: 'Pandya',
-            email: 'Rama@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'Inactive'
-        },
-        {
-            firstName: 'Raja',
-            lastName: 'Krishna',
-            email: 'raja@gmail.com',
-            phone: '9704399315',
-            designation: 'Trainee',
-            status: 'active'
-        }
-    ]
+//         },
+//         {
+//             firstName: 'Subha',
+//             lastName: 'Vellapandi',
+//             email: 'subha@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'active'
+//         },
+//         {
+//             firstName: 'Shruthi',
+//             lastName: 'Muthu',
+//             email: 'Shruthi@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'Inactive'
+//         },
+//         {
+//             firstName: 'Manisha',
+//             lastName: 'Akula',
+//             email: 'Manisha@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'Inactive'
+//         },
+//         {
+//             firstName: 'Priyanka',
+//             lastName: 'Chittibabu',
+//             email: 'priyanka@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'Inactive'
+//         },
+//         {
+//             firstName: 'Mohana',
+//             lastName: 'Manohar',
+//             email: 'mohana@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'active'
+//         },
+//         {
+//             firstName: 'Rama',
+//             lastName: 'Pandya',
+//             email: 'Rama@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'Inactive'
+//         },
+//         {
+//             firstName: 'Raja',
+//             lastName: 'Krishna',
+//             email: 'raja@gmail.com',
+//             phone: '9704399315',
+//             designation: 'Trainee',
+//             status: 'active'
+//         }
+//     ]
+// }
+
+const client = require('../db');
+
+exports.getUsers = async () => {
+
+    const result = await client.query('select * from userdetails')
+    return result.rows;
+
+}
+
+exports.getUsersByID = async (id) => {
+    const result = await client.query(`select * from userdetails where id=${id}`)
+    return result.rows[0];
+}
+
+exports.insert = async (user) => {
+    try {
+        const query = 'INSERT INTO userdetails(username,email,mobile,designation,firstname,lastname,status,isdeleted) VALUES($1,$2,$3,$4,$5,$6,$7,$8)'
+        values = [user.username, user.email, user.mobile, user.designation, user.firstname, user.lastname, user.status, user.isdeleted];
+        await client.query(query, values)
+    }
+    catch (err) {
+        console.log(err);
+        throw 'Unable to insert user'
+    }
 }
