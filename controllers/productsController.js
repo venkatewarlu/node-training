7eaadcb39c4b75291c7718f156bb7918a9515bd2
venkateswarlu <nallapadu.venkateswarlu@gmail.com>
2019-04-03 10:07:21 +0530

    products management

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,24 +1,44 @@
 const router = require('express').Router();
 const productService = require('../services/productsService');
 
-router.get('/', (req, res) => {
-    const productslist = productService.getProducts()
+router.get('/', async (req, res) => {
+    const productslist = await productService.getProducts()
     // res.json({ message: 'Product Route Successful' })
 
     res.json({ products: productslist })
 
 })
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    console.log(`Product ID is ${id}`)
-    res.json({ message: 'Individual Product Route Successful' })
+// router.get('/:id', (req, res) => {
+//     const id = req.params.id
+//     console.log(`Product ID is ${id}`)
+//     res.json({ message: 'Individual Product Route Successful' })
+
+// })
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await productService.getProductsByID(parseInt(id))
+    res.json({ product: product });
 
 })
 
 router.post('/', (req, res) => {
     console.log(JSON.stringify(req.body, undefined, 4));
     res.json({ message: "Products post method successful" })
+})
+
+router.post('/:userName/:manufacturerID', async (req, res) => {
+    try {
+        const { userName, manufacturerID } = req.params;
+        await productService.insert(userName, parseInt(manufacturerID), req.body);
+        res.json({ message: "Product Successfully added" })
+
+    }
+    catch (err) {
+        res.status(422).json({ message: err });
+    }
+
 })
 
 module.exports = router
--- services/productsService.js
+++ services/productsService.js
@@ -1,19 +1,48 @@
-exports.getProducts = () => {
-    return [
-        {
-            "name": "One Plus 5",
-            "company": "China",
-            "manufacturer": "One Plus",
-            "owner": "kyangyong",
-            "price": "35000"
-        },
-        {
-            "name": "One Plus 5",
-            "company": "China",
-            "manufacturer": "One Plus",
-            "owner": "kyangyong",
-            "price": "35000"
+const client = require('../db');
 
-        }
-    ]
+// exports.getProducts = () => {
+//     return [
+//         {
+//             "name": "One Plus 5",
+//             "company": "China",
+//             "manufacturer": "One Plus",
+//             "owner": "kyangyong",
+//             "price": "35000"
+//         },
+//         {
+//             "name": "One Plus 5",
+//             "company": "China",
+//             "manufacturer": "One Plus",
+//             "owner": "kyangyong",
+//             "price": "35000"
+
+//         }
+//     ]
+// }
+
+exports.getProducts = async () => {
+
+    const result = await client.query('select * from products1')
+    return result.rows;
+
 }
+
+exports.getProductsByID = async (id) => {
+    const result = await client.query(`select * from products1 where id=${id}`)
+    return result.rows[0];
+}
+
+
+exports.insert = async (userName, manufacturerID, product) => {
+    try {
+        const query = 'INSERT INTO products1(name,code,manufacturer,price,description,quantity,createdat,createdby) VALUES($1,$2,$3,$4,$5,$6,$7,$8)'
+        values = [product.name, product.code, manufacturerID, product.price, product.description, product.quantity, new Date(), userName];
+        await client.query(query, values)
+    }
+    catch (err) {
+        console.log(err);
+        throw 'Unable to insert product'
+    }
+}
+
+
